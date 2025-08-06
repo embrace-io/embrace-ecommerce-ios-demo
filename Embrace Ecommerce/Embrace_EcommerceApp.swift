2c5ee861a930af6d9bef402ff84df6180608e3e2
Sergio Rodriguez <103063371+sergiothinks2@users.noreply.github.com>
2025-08-06 10:14:52 -0700

    Initial Commit

---
+++ Embrace Ecommerce/Embrace_EcommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Embrace_EcommerceApp.swift
+//  Embrace Ecommerce
+//
+//  Created by Sergio Rodriguez on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Embrace_EcommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
