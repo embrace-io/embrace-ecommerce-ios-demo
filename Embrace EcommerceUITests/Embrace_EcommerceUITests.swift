f55855e6540c1039042c03db6f42e06e5ce3ca55
David Rifkin <50370157+davidlawrencer@users.noreply.github.com>
2025-09-17 13:39:02 -0500

    Add UITest target and stubs (#2)

---
+++ Embrace EcommerceUITests/Embrace_EcommerceUITests.swift
@@ -0,0 +1,37 @@
+//
+//  Embrace_EcommerceUITests.swift
+//  Embrace EcommerceUITests
+//
+//  Created by David Rifkin on 9/17/25.
+//
+
+import XCTest
+
+final class Embrace_EcommerceUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    @MainActor
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    @MainActor
+    func testLaunchPerformance() throws {
+        // This measures how long it takes to launch your application.
+        measure(metrics: [XCTApplicationLaunchMetric()]) {
+            XCUIApplication().launch()
+        }
+    }
+}
